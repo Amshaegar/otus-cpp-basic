9d7864a1c893fbdf377173fdcfe1a0a748d1f7f0
Maxim Fedotov <fedotovmo@gmail.com>
2025-05-05 20:56:59 +0300

    Multithread collision crack

--- main.cpp
+++ main.cpp
@@ -1,14 +1,44 @@
 #include <algorithm>
+#include <atomic>
 #include <iostream>
 #include <limits>
+#include <mutex>
+#include <thread>
 #include <vector>
 
 #include "CRC32.hpp"
 #include "IO.hpp"
 
+std::atomic_bool found = false;
+std::vector<char> answer;
+std::mutex answerMutex;
+
 /// @brief Переписывает последние 4 байта значением value
 void replaceLastFourBytes(std::vector<char> &data, uint32_t value) {
   std::copy_n(reinterpret_cast<const char *>(&value), 4, data.end() - 4);
+}
+
+void calc(std::vector<char> data,
+          const size_t batchBegin,
+          const size_t batchEnd,
+          const uint32_t originalCrc32){
+  for (size_t i = batchBegin; i < batchEnd; ++i) {
+    if (found) {
+        return;
+    }
+
+    // Заменяем последние четыре байта на значение i
+    replaceLastFourBytes(data, (uint32_t)i);
+    // Вычисляем CRC32 текущего вектора result
+    auto currentCrc32 = crc32(data.data(), data.size());
+
+    if (currentCrc32 == originalCrc32) {
+      found = true;
+      std::lock_guard<std::mutex> guard(answerMutex);
+      answer = data;
+      return;
+    }
+  }
 }
 
 /**
@@ -35,24 +65,41 @@ std::vector<char> hack(const std::vector<char> &original,
    * В качестве доп. задания устраните избыточные вычисления
    */
   const size_t maxVal = std::numeric_limits<uint32_t>::max();
-  for (size_t i = 0; i < maxVal; ++i) {
-    // Заменяем последние четыре байта на значение i
-    replaceLastFourBytes(result, uint32_t(i));
-    // Вычисляем CRC32 текущего вектора result
-    auto currentCrc32 = crc32(result.data(), result.size());
 
-    if (currentCrc32 == originalCrc32) {
-      std::cout << "Success\n";
-      return result;
-    }
-    // Отображаем прогресс
-    if (i % 1000 == 0) {
-      std::cout << "progress: "
-                << static_cast<double>(i) / static_cast<double>(maxVal)
-                << std::endl;
-    }
+  unsigned int t = std::thread::hardware_concurrency();
+  if (t == 0) {
+    t=1;
   }
-  throw std::logic_error("Can't hack");
+
+  std::cout << "Calculate in " << t << " concurrent threads." << std::endl;
+  size_t batch_size = maxVal / t;
+  std::cout << "Batch size: " << batch_size << std::endl;
+  std::cout << "Max value: " << maxVal << std::endl;
+
+  std::vector<std::thread> threads;
+  for(size_t i = 0; i < t ; ++i) {
+    size_t batchBegin = i*batch_size;
+    size_t batchEnd = (i == t - 1) ? maxVal : batchBegin + batch_size;
+    std:: cout << "Batch begin: " << batchBegin << std::endl;
+    std:: cout << "Batch end: " << batchEnd << std::endl;
+    threads.emplace_back(std::thread(calc,
+                                     result,
+                                     batchBegin,
+                                     batchEnd,
+                                     originalCrc32));
+  }
+
+  for(unsigned int i = 0; i < t ; ++i) {
+    threads.at(i).join();
+  }
+
+  if (!found) {
+    throw std::logic_error("Can't hack");
+  }
+
+  std::cout << "Success\n";
+
+  return answer;
 }
 
 int main(int argc, char **argv) {
